74e71f77f4520c4232ab0ae4c5fee47257404434
TFYXGG <861578054@qq.com>
2019-04-18 21:44:09 +0800

    添加数据库操作类

---
+++ 项目3/Database.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "IdataBase.h"
+#include <stdio.h>
+#include <assert.h>
+#include <Windows.h>
+#include <sql.h>
+#include <sqlext.h>
+
+
+class Database :public IdataBase
+{
+public:
+	//构建数据库连接，参数：数据源名称 用户名 密码
+	Database(string serverName,string userName,string passWorld);
+	vector<vector<string>> query(string sql, vector<DataType> type);
+	bool upData(string sql);
+	~Database();
+private:
+	HENV henv;
+	SQLHDBC hdbc;
+	//SQLHSTMT stmt;
+};
