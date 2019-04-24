44fb6ad4f29b6b5d6ee63e44bcb2ab1ee59c0ca4
TFYXGG <861578054@qq.com>
2019-04-24 13:57:17 +0800

    logic 类重写

--- 项目3/logic.cpp
+++ 项目3/logic.cpp
@@ -1,109 +1,43 @@
+
 #include "logic.h"
 
-Logic::Logic(IdataBase * db)
+
+logic::logic(IdataBase * db)
 {
 	this->db = db;
 }
 
-int Logic::add(vector<string> information)
+bool logic::land(string nuerName, string passWorld)
 {
-	if (information.empty())
-		return -1;
-	stringstream ss;
-	ss << "insert into " << BasicTable << " values(";
-	ss << information.at(0);
-	for (int i = 1; i < information.size(); i++)
-	{
-		ss << "," << information.at(i);
-	}
-	ss << ")";
-	return db->upData(ss.str());
+	return false;
 }
 
-int Logic::del(int id)
+vector<vector<string>> logic::sightseeing()
 {
-	stringstream ss;
-	ss << "delete from " << BasicTable << " where " << DB_BT_id << " = " << id;
-	return db->upData(ss.str());
+	return vector<vector<string>>();
 }
 
-int Logic::del(string name)
+bool logic::addAttractions(string name, float fare, string address)
 {
-	if (name.empty())
-		return -1;
-	stringstream ss;
-	ss << "delete from " << BasicTable << " where " << DB_BT_name << " = " << name;
-	return db->upData(ss.str());
+	return false;
 }
 
-int Logic::del(vector<int> ids)
-{	
-	if (ids.empty())
-		return -1;
-	stringstream ss;
-	ss << "delete from " << BasicTable << " where 1=0 ";
-	for (int i = 0; i < ids.size(); i++)
-	{
-		ss << "or " << DB_BT_id << " =" << ids.at(i) << " ";
-	}
-	return db->upData(ss.str());
+bool logic::modify(int id, string newName, float newFare, string newAddress)
+{
+	return false;
 }
 
-int Logic::updata(int id, vector<map<string, string>> information)
+bool logic::remove(int id)
 {
-	if (information.empty())
-		return -1;
-	stringstream ss;
-	ss << "update " << BasicTable << " set ";
-	ss << information[0]["key"] << " = " << information[0]["value"];
-	for (int i = 1; i < information.size(); i++) 
-	{
-		ss << "," << information[i]["key"] << " = " << information[i]["value"];
-	}
-	
-	ss << " where " << DB_BT_id << " = " << id;
-	return db->upData(ss.str());
+	return false;
 }
 
-vector<vector<string>> Logic::find(vector<map<string, string>> information)
+vector<vector<string>> logic::monthlySummary(string month)
 {
-	if (information.empty())
-		return vector<vector<string>>();
-	stringstream ss;
-	ss << "select * from " << BasicTable << " where 1=1";
-	for (int i = 0; i < information.size(); i++)
-	{
-		ss << " and " << information[i]["key"] << information[i]["relation"] << information[i]["value"];
-	}
-	return db->query(ss.str());
-}
-int Logic::del(vector<string> names)
-{
-	if (names.empty())
-		return -1;
-	stringstream ss;
-	ss << "delete from " << BasicTable << " where 1=0";
-	for (int i = 0; i < names.size(); i++)
-	{
-		ss << " or " << DB_BT_name << " = " << names.at(i);
-	}
-	return db->upData(ss.str());
+	return vector<vector<string>>();
 }
 
-vector<vector<string>> Logic::find(string name, int month)
-{
-	stringstream ss;
-	ss << "select * from " << SummaryTable;
-	if (name.empty())
-		return db->query(ss.str());
-	ss << " where " << DB_ST_name << " = " << name;
-	if(month = -1)
-		return db->query(ss.str());
-	ss << " " << DB_ST_month << " = " << month;
-	return db->query(ss.str());
-}
-
-Logic::~Logic()
+logic::~logic()
 {
 	delete db;
 }
--- 项目3/logic.h
+++ 项目3/logic.h
@@ -1,65 +1,32 @@
 #pragma once
-#include "Database.h"
-#include <map>
-#include <sstream>
 
-//基本表，表名
-const string BasicTable = "TourismPovertyAlleviationFundManagementSystem";
-//汇总表表名
-const string SummaryTable = "summary";
-//各字段名
-//id
-const string DB_BT_id = "id";
-//景点名称
-const string DB_BT_name = "spot_name";
-//票价
-const string DB_BT_fare = "fare";
-//游客量
-const string DB_BT_tourist = "Amount_of_tourists";
-//全票量
-const string DB_BT_full = "Full_ticket_num";
-//折扣票量
-const string DB_BT_discount = "discount_num";
-//捐款额
-const string DB_BT_donation = "Total_donation";
-//日期
-const string DB_BT_date = "date";
-//景点名
-const string DB_ST_name = "spot_name";
-//月份
-const string DB_ST_month = "month";
-//捐款金额
-const string DB_ST_donation = "contribution_amount";
-//游客数量
-const string DB_ST_tourist = "amount_of_tourists";
+#include "IdataBase.h"
 
-
-class Logic
+class logic
 {
 public:
-	Logic(IdataBase *db);
+	//获取实例
+	logic(IdataBase *db);
 
-	int add(vector<string> information);
-	//根据id删除
-	int del(int id);
-	int del(vector<int> ids);
-	//修改 id不变 
-	int updata(int id, vector<map<string, string>> information);
-	//查找
-	//返回操作影响的行数 输入参数为键值对数组 information[0]{key = name,relation = "=" 
-	//value = "大明宫"},information[1]{key = address,relation = "=" ,value = "西安"}
-	vector<vector<string>> find(vector<map<string, string>> information);
+	//登陆	登陆名 登陆密码
+	bool land(string nuerName, string passWorld);
 
-	//月汇总
-	//删除 删除整个景点的所有信息
-	int del(string name);
-	int del(vector<string> names);
-	//查找景点月信息
-	//查找所有信息
-	//month = -1时查找该景点所有月份数据 name为空时查找所有景点所有数据
-	vector<vector<string>> find(string name = "", int month = -1);
+	//景点查询
+	vector<vector<string>> sightseeing();
 
-	~Logic();
+	//增加景点	景点名 票价 地址
+	bool addAttractions(string name, float fare, string address);
+
+	//修改景点信息
+	bool modify(int id, string newName, float newFare, string newAddress);
+
+	//删除景点
+	bool remove(int id);
+
+	//月总概况
+	vector<vector<string>> monthlySummary(string month);
+
+	~logic();
 private:
 	IdataBase *db;
 };
